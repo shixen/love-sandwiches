3fe4c8179091290d4465a842a2cefbbdb391f9b3
niklas eriksson <niklaseriksson32@hotmail.com>
2024-02-13 13:06:39 +0000

    retrive latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
-
+from pprint import pprint
+"""
+pip3 install gspread google-auth
+"""
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -60,6 +63,26 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("sales workshet updated successfully.")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    compare sales with stock and calculate the surplus for each item type.
+    """
+    print("calculating surplus data.....")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+
+def main():
+    """
+    run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+  
+print("welcome to love sandwiches data atuomation")
+main()
